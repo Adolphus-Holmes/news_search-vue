09d4d57302e4135bcb340067ab2856e906bdf146
Adolphus-Holmes <c1559131063@qq.com>
2021-04-11 01:35:20 +0800

    优化显示

--- src/views/Cache.vue
+++ src/views/Cache.vue
@@ -3,13 +3,13 @@
         <el-container>
             <el-header style="height:120px;background:#f8f9fa;text-align:left;padding:16px 28px 24px 28px">
                 <el-row>
-                  <el-col :span="24">来源链接:<el-link :href=Article.url target="_blank">{{Article.url}}</el-link></el-col>
+                  <el-col :span="24">来源链接:&nbsp;<el-link :href=Article.url target="_blank" style="font-size: 16px;">{{Article.url}}</el-link></el-col>
                 </el-row>
                 <el-row style="padding-top:10px"> 
-                  <el-col :span="24">抓取时间:{{Article.crawl_date.replace(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d+)/,"$1年$2月$3日 $4:$5:$6 GMT+8")}}</el-col>
+                  <el-col :span="24">抓取时间:&nbsp;{{Article.crawl_date.replace(/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).(\d+)/,"$1年$2月$3日 $4:$5:$6 GMT+8")}}</el-col>
                 </el-row>
                 <el-row style="padding-top:10px"> 
-                  <el-col :span="24">网站名称:{{Article.domain}}</el-col>
+                  <el-col :span="24">网站名称:&nbsp;{{Article.domain}}</el-col>
                 </el-row>
             </el-header>
             <el-main style="margin-left: 39vh;width: 60%;line-height:2;">
@@ -66,8 +66,7 @@ export default {
                 }
             })
             this.Article = response.data;
-            this.Article.text = this.Article.text.replace(/。\s{3,}/g,"。\n　　");
-            console.log(this.Article.text);
+            this.Article.text = "\t"+this.Article.text.trim().replace(/。\s{3,}/g,"。\n　　");
         },
     },
     // 生命周期函数
